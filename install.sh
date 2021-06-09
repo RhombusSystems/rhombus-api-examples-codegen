82697879d22aec3d6aa6f65ce0add61594f7c608
Bricktheworld <brandon.shihabi@gmail.com>
2021-06-09 13:37:54 -0700

    Fixed the clone link for install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # PLACEHOLDER
 rm -rf RhombusTypescriptAPI
-git clone git@github.com:Bricktheworld/RhombusTypescriptAPI.git ._RhombusTypescriptAPISrc
+git clone git@github.com:RhombusSystems/rhombus-api-examples-codegen.git ._RhombusTypescriptAPISrc
 
 cd ._RhombusTypescriptAPISrc
 ./build.sh
