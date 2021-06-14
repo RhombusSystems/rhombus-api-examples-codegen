cc70d031b7c560bc97a48e0f2a3fad6d93cdac7f
Bricktheworld <brandon.shihabi@gmail.com>
2021-06-14 13:46:38 -0700

    Fixed install scripts

--- python/clean.sh
+++ python/clean.sh
@@ -1,2 +1,2 @@
-rm -rf RhombusPythonAPI
+rm -rf RhombusAPI
 rm -rf src
--- python/install.sh
+++ python/install.sh
@@ -1,9 +1,9 @@
-rm -rf RhombusPythonAPI
+rm -rf RhombusAPI
 git clone git@github.com:RhombusSystems/rhombus-api-examples-codegen.git ._RhombusPythonAPISrc
 
 cd ._RhombusPythonAPISrc/python/
 ./build.sh
-cp -r RhombusPythonAPI ../../RhombusPythonAPI
+cp -r RhombusAPI ../../RhombusAPI
 cd ..
 cd ..
 rm -rf ._RhombusPythonAPISrc
