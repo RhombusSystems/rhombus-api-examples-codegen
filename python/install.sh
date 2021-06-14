9fe68c5f345a2b2cd1a2b1347bf35c3044972a49
Bricktheworld <brandon.shihabi@gmail.com>
2021-06-14 13:17:54 -0700

    Fixed a few issues with python scripts

--- python/install.sh
+++ python/install.sh
@@ -1,10 +1,9 @@
-rm -rf RhombusTypescriptAPI
-git clone git@github.com:RhombusSystems/rhombus-api-examples-codegen.git ._RhombusTypescriptAPISrc
+rm -rf RhombusPythonAPI
+git clone git@github.com:RhombusSystems/rhombus-api-examples-codegen.git ._RhombusPythonAPISrc
 
-cd ._RhombusTypescriptAPISrc/python/
+cd ._RhombusPythonAPISrc/python/
 ./build.sh
 cp -r RhombusPythonAPI ../../RhombusPythonAPI
 cd ..
 cd ..
 rm -rf ._RhombusTypescriptAPISrc
-
