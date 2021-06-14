2b96d8f4e498dd7798ac9387768fe7f3e8111f2b
Bricktheworld <brandon.shihabi@gmail.com>
2021-06-14 09:52:21 -0700

    Moved all files into typescript/ directory, fixed build scripts to match this change, and added root README.md which is still a work in progress

--- typescript/build.sh
+++ typescript/build.sh
@@ -1,5 +1,5 @@
 cd src
-wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.26/swagger-codegen-cli-3.0.26.jar -O swagger-codegen-cli.jar
+curl https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.26/swagger-codegen-cli-3.0.26.jar --output swagger-codegen-cli.jar
 java -jar swagger-codegen-cli.jar generate -i https://api2.rhombussystems.com/api/openapi/public.json -l typescript-fetch -o .
 rm api_test.spec.ts git_push.sh swagger-codegen-cli.jar
 npm install
--- typescript/install.sh
+++ typescript/install.sh
@@ -1,10 +1,10 @@
-# PLACEHOLDER
 rm -rf RhombusTypescriptAPI
 git clone git@github.com:RhombusSystems/rhombus-api-examples-codegen.git ._RhombusTypescriptAPISrc
 
-cd ._RhombusTypescriptAPISrc
+cd ._RhombusTypescriptAPISrc/typescript/
 ./build.sh
-cp -r RhombusTypescriptAPI ../RhombusTypescriptAPI
+cp -r RhombusTypescriptAPI ../../RhombusTypescriptAPI
+cd ..
 cd ..
 rm -rf ._RhombusTypescriptAPISrc
 
